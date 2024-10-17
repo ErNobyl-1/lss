9a00da01b324390dacfcad64db5ee47b04efebbb
ErNobyl <ernobyl@outlook.de>
2024-10-17 17:03:13 +0200

    removed detect retina

--- LSSRedesign.user.js
+++ LSSRedesign.user.js
@@ -95,52 +95,42 @@ TODO
         map.invalidateSize();
         // add changable tile layers
         var BaseMapDE_Color = L.tileLayer('https://sgx.geodatenzentrum.de/wmts_basemapde/tile/1.0.0/de_basemapde_web_raster_farbe/default/GLOBAL_WEBMERCATOR/{z}/{y}/{x}.png', {
-            detectRetina: true,
             maxZoom: 19,
             attribution: 'Map data: &copy; <a href="http://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a>'
         });
         var BaseMapDE_Grey = L.tileLayer('https://sgx.geodatenzentrum.de/wmts_basemapde/tile/1.0.0/de_basemapde_web_raster_grau/default/GLOBAL_WEBMERCATOR/{z}/{y}/{x}.png', {
-            detectRetina: true,
             maxZoom: 19,
             attribution: 'Map data: &copy; <a href="http://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a>'
         });
         var osmhot = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-            detectRetina: true,
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
         }).addTo(map);
         var osmgermany = L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
-            detectRetina: true,
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         });
         var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            detectRetina: true,
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         });
         var osmtopo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-            detectRetina: true,
             maxZoom: 15,
             attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
         });
         var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
-            detectRetina: true,
             maxZoom: 19,
             subdomains:['mt0','mt1','mt2','mt3']
         });
         var googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
-            detectRetina: true,
             maxZoom: 19,
             subdomains:['mt0','mt1','mt2','mt3']
         });
         var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-            detectRetina: true,
             maxZoom: 19,
             subdomains:['mt0','mt1','mt2','mt3']
         });
         var googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
-            detectRetina: true,
             maxZoom: 19,
             subdomains:['mt0','mt1','mt2','mt3']
         });
